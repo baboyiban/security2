08f7d29b6e8f52dd389fde293bb1ea770ff42dc0
baboyiban <chl11wq12@gmail.com>
2023-01-06 11:57:54 +0900

    login, logout success

--- src/main/java/com/ezen/demo/controller/SecEmpController.java
+++ src/main/java/com/ezen/demo/controller/SecEmpController.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/sec")
 public class SecEmpController {
-	@GetMapping("/index")
-	public String loginForm() {
+	@GetMapping("/loginForm")
+	public String loginForm(String logout) {
+		System.out.println("logout:"+logout);
+		
 		return "thymeleaf/index";
+	}
+	@GetMapping("/menu")
+	public String menuForm() {
+		return "thymeleaf/menu";
 	}
 }
